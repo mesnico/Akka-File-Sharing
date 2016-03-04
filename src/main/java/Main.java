c4a959afaa6d70a1c65bd8c7ace8d4f635f5541a
mesnico <nicola.messina93@gmail.com>
2016-03-04 17:59:10 +0100

    Added Akka Cluster support

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nicky
+ */
+public class Main {
+    public static void main(String[] args) {
+        if (args.length == 0)
+            startup(new String[] { "2551", "2552", "0" });
+        else
+            startup(args);
+    }
+
+    public static void startup(String[] ports) {
+        for (String port : ports) {
+        // Override the configuration of the port
+            Config config = ConfigFactory.parseString(
+                "akka.remote.netty.tcp.port=" + port).withFallback(
+                ConfigFactory.load());
+
+        // Create an Akka system
+            ActorSystem system = ActorSystem.create("ClusterSystem", config);
+
+        // Create an actor that handles cluster domain events
+            system.actorOf(Props.create(ClusterListener.class),
+                "clusterListener"+port);
+
+        }
+    }
+}
