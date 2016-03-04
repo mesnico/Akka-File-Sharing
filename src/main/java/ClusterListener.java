a0884b967c08ed974c1d96b3482a542d178f107f
mesnico <nicola.messina93@gmail.com>
2016-03-04 23:33:33 +0100

    Added the Member Map (TreeMap) inside each actor

--- src/main/java/ClusterListener.java
+++ src/main/java/ClusterListener.java
@@ -2,62 +2,99 @@
 import akka.actor.UntypedActor;
 import akka.cluster.Cluster;
 import akka.cluster.ClusterEvent;
+import akka.cluster.ClusterEvent.CurrentClusterState;
 import akka.cluster.ClusterEvent.MemberEvent;
 import akka.cluster.ClusterEvent.MemberRemoved;
 import akka.cluster.ClusterEvent.MemberUp;
 import akka.cluster.ClusterEvent.UnreachableMember;
+import akka.cluster.Member;
+import akka.cluster.MemberStatus;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.TreeMap;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author nicky
  */
 public class ClusterListener extends UntypedActor {
-  LoggingAdapter log = Logging.getLogger(getContext().system(), this);
-  Cluster cluster = Cluster.get(getContext().system());
 
-  //subscribe to cluster changes
-  @Override
-  public void preStart() {
-    //#subscribe
-    cluster.subscribe(getSelf(), ClusterEvent.initialStateAsEvents(), 
-        MemberEvent.class, UnreachableMember.class);
-    //#subscribe
-  }
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+    Cluster cluster = Cluster.get(getContext().system());
+    TreeMap<BigInteger, Member> membersMap = new TreeMap();
 
-  //re-subscribe when restart
-  @Override
-  public void postStop() {
-    cluster.unsubscribe(getSelf());
-  }
-
-  @Override
-  public void onReceive(Object message) {
-    if (message instanceof MemberUp) {
-      MemberUp mUp = (MemberUp) message;
-      log.info("Member is Up: {}", mUp.member());
-
-    } else if (message instanceof UnreachableMember) {
-      UnreachableMember mUnreachable = (UnreachableMember) message;
-      log.info("Member detected as unreachable: {}", mUnreachable.member());
-
-    } else if (message instanceof MemberRemoved) {
-      MemberRemoved mRemoved = (MemberRemoved) message;
-      log.info("Member is Removed: {}", mRemoved.member());
-
-    } else if (message instanceof MemberEvent) {
-      // ignore
-
-    } else {
-      unhandled(message);
+    //subscribe to cluster changes
+    @Override
+    public void preStart() {
+        //#subscribe
+        cluster.subscribe(getSelf(), ClusterEvent.initialStateAsEvents(),
+                MemberEvent.class, UnreachableMember.class);
+        //#subscribe
     }
 
-  }
+    //re-subscribe when restart
+    @Override
+    public void postStop() {
+        cluster.unsubscribe(getSelf());
+    }
+
+    @Override
+    public void onReceive(Object message) {
+        if (message instanceof MemberUp) {
+            MemberUp mUp = (MemberUp) message;
+            log.info("{} -->Member is Up: {}", getSelf().path().address(), mUp.member().address());
+
+            //inserts the new member in the map
+            membersMap.put(computeMemberHash(mUp.member()), mUp.member());
+            System.out.println(membersMap);
+
+        } else if (message instanceof UnreachableMember) {
+            UnreachableMember mUnreachable = (UnreachableMember) message;
+            log.info("Member detected as unreachable: {}", mUnreachable.member());
+
+        } else if (message instanceof MemberRemoved) {
+            MemberRemoved mRemoved = (MemberRemoved) message;
+            log.info("Member is Removed: {}", mRemoved.member());
+            
+            //removes the member in the map
+            membersMap.remove(computeMemberHash(mRemoved.member()));
+
+        //message sent when the new member arrives in the cluster. The map has to be immediately filled with the current state
+        } else if (message instanceof CurrentClusterState) {
+            CurrentClusterState state = (CurrentClusterState) message;
+            for (Member member : state.getMembers()) {
+                if (member.status().equals(MemberStatus.up())) {
+                    membersMap.put(computeMemberHash(member),member);
+                }
+            }
+
+        } else if (message instanceof MemberEvent) {
+            // ignore
+
+        } else {
+            unhandled(message);
+        }
+    }
+
+    private BigInteger computeMemberHash(Member member) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.reset();
+            md.update(member.address().toString().getBytes("UTF-8"));
+            return new BigInteger(md.digest());
+        } catch (NoSuchAlgorithmException e) {
+            return BigInteger.ZERO;
+        } catch (UnsupportedEncodingException e) {
+            return BigInteger.ZERO;
+        }
+    }
 }
