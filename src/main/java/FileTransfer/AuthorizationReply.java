48389598dbaba7fe31666a8cdb4bfc244664a619
unknown <unknown@unknown.com>
2016-04-09 10:31:03 +0200

    Tcp structures added for testing pourpose

---
+++ src/main/java/FileTransfer/AuthorizationReply.java
@@ -0,0 +1,26 @@
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alessandro
+ */
+
+
+public class AuthorizationReply implements Serializable {
+    EnumAuthorizationReply response;
+    
+    public AuthorizationReply(EnumAuthorizationReply response){
+        this.response = response;
+    }
+    
+    public EnumAuthorizationReply getResponse(){
+        return response;
+    }
+}
