2d32f9796ef436c5f0356528015642075f2b8097
Francesco Culcasi <fpculcasi@gmail.com>
2016-04-11 17:23:12 +0200

    Correct a typo on the previous commit
    Also removed a not used message, and moved "SendModifyRequest" into GUI.messages

--- src/main/java/GUI/messages/SendModifyRequest.java
+++ src/main/java/GUI/messages/SendModifyRequest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ClusterListenerActor.messages;
+package GUI.messages;
 
 import FileTransfer.FileModifier;
 import java.io.Serializable;
